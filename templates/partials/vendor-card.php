8cf1cc7a2ad3c3b0327421921227976bfa5de1c1
Nitin Patil <pallavi@wbcomdesigns.com>
2026-03-18 12:35:47 +0530

    Add details icons
    https://3.basecamp.com/5798509/buckets/45156734/card_tables/cards/9691659012

--- templates/partials/vendor-card.php
+++ templates/partials/vendor-card.php
@@ -119,14 +119,18 @@ do_action( 'wpss_before_vendor_card', $vendor_id );
 		<?php endif; ?>
 
 		<li>
-			<span class="wpss-detail-icon wpss-icon-calendar"></span>
+			<span class="wpss-detail-icon wpss-icon-calendar">
+				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 11H4V19H20V11ZM7 5H4V9H20V5H17V7H15V5H9V7H7V5Z"></path></svg>
+			</span>
 			<span class="wpss-detail-label"><?php esc_html_e( 'Member since', 'wp-sell-services' ); ?></span>
 			<span class="wpss-detail-value"><?php echo esc_html( wp_date( 'M Y', strtotime( $member_since ) ) ); ?></span>
 		</li>
 
 		<?php if ( $response_time ) : ?>
 			<li>
-				<span class="wpss-detail-icon wpss-icon-clock"></span>
+				<span class="wpss-detail-icon wpss-icon-clock">
+					<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM13 12H17V14H11V7H13V12Z"></path></svg>
+				</span>
 				<span class="wpss-detail-label"><?php esc_html_e( 'Avg. Response', 'wp-sell-services' ); ?></span>
 				<span class="wpss-detail-value"><?php echo esc_html( $response_time ); ?></span>
 			</li>
@@ -134,7 +138,9 @@ do_action( 'wpss_before_vendor_card', $vendor_id );
 
 		<?php if ( $is_online ) : ?>
 			<li>
-				<span class="wpss-detail-icon wpss-icon-activity"></span>
+				<span class="wpss-detail-icon wpss-icon-activity">
+					<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM13 12H17V14H11V7H13V12Z"></path></svg>
+				</span>
 				<span class="wpss-detail-label"><?php esc_html_e( 'Last Delivery', 'wp-sell-services' ); ?></span>
 				<span class="wpss-detail-value"><?php echo esc_html( wpss_time_ago( $is_online ) ); ?></span>
 			</li>
